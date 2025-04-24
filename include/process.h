6e2e28b2e7f0064375012daad1bc1ffb8b9f9c9a
Joody <joodymohamed75@gmail.com>
2025-04-24 18:48:02 +0200

    added methods

--- include/process.h
+++ include/process.h
@@ -3,14 +3,13 @@
 
 typedef struct {
     int pid;
-    char state[10];       // e.g., "Ready", "Running", "Blocked"
+    char state[10];       // Ready, Running, Blocked
     int priority;
     int programCounter;
     int memLowerBound;
     int memUpperBound;
 } PCB;
 
-// Function prototypes
 PCB createPCB(int pid, int priority, int memLower, int memUpper);
 
 void printPCB(PCB pcb);
